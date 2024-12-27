72b80a432bf18ff0babaaa6cdc69e1954195a8bc
mataager <omarbussmo077@gmail.com>
2024-12-27 20:13:40 +0200

    Add files via upload

---
+++ assets/js/static.js
@@ -0,0 +1,51 @@
+const url = "https://matager-f1f00-default-rtdb.firebaseio.com";
+const storeurl = "https://matager-f1f00-default-rtdb.firebaseio.com";
+const storename = "Prof Store";
+const uid = "R0jsmpHvS5U7RkRyd6Rj7YbSJvo1";
+const storeuid = "R0jsmpHvS5U7RkRyd6Rj7YbSJvo1";
+const storePhoneNum = "+20 1097 755 498";
+const storePhoneNum2 = "";
+const storeEmail = "yassinsalman2007@gmail.com";
+const storeGender = [];
+const storeType = "clothing"; // clothing or footwear
+const freeshipping = "2000";
+//images url
+const pannerurl = "https://i.imgur.com/TwU6xKz.jpeg";
+//
+const mentopurl = "";
+const menbottomurl = "";
+const menfootwareurl = "";
+const menshoseseurl = "";
+const menslippersurl = "";
+const menflipflopsurl = "";
+const mensandalsurl = "";
+const menLoafersurl = "";
+const menMoccasinsurl = "";
+const menclassicshoseurl = "";
+const menbootsurl = "";
+//
+const womentopurl = "";
+const womenbottomurl = "";
+const womenfootwareurl = "";
+const womenshoseseurl = "";
+const womenheelsurl = "";
+const womenslippersurl = "";
+const womenflipflopsurl = "";
+const womensandalsurl = "";
+const womenLoafersurl = "";
+const womenMoccasinsurl = "";
+const womenbootsurl = "";
+//
+const kidtopurl = "";
+const kidbottomurl = "";
+const kidfootwareurl = "";
+const kidshoseseurl = "";
+const kidslippersurl = "";
+const kidsandalsurl = "";
+const kidbootssurl = "";
+//
+const menscollectionurl = "";
+const womencollectionurl = "";
+const kidscollectionurl = "";
+const newarrivalurl = "https://i.imgur.com/an9ECvD.jpeg";
+const saleurl = "https://i.imgur.com/nQdi6SN.jpeg";
---
+++ manage/js/static.js
@@ -0,0 +1,9 @@
+const url = "https://matager-f1f00-default-rtdb.firebaseio.com";
+const storeurl = "https://matager-f1f00-default-rtdb.firebaseio.com";
+const uid = localStorage.getItem("uid");
+const matager_percentage = 0.03;
+const clientId = "44d832fb77a6b9d";
+//for handling the page title
+const storetitle = "Prof Dashboard";
+document.getElementById("title").innerHTML = storetitle;
+//
