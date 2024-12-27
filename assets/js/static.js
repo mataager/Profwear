4685ecbce3174c9c6e5ef87ec73f62ca03663a70
mataager <omarbussmo077@gmail.com>
2024-12-27 20:37:43 +0200

    Update static.js

--- assets/js/static.js
+++ assets/js/static.js
@@ -1,8 +1,8 @@
 const url = "https://matager-f1f00-default-rtdb.firebaseio.com";
 const storeurl = "https://matager-f1f00-default-rtdb.firebaseio.com";
 const storename = "Prof Store";
-const uid = "R0jsmpHvS5U7RkRyd6Rj7YbSJvo1";
-const storeuid = "R0jsmpHvS5U7RkRyd6Rj7YbSJvo1";
+const uid = "OtCqKZEsliZHUhvkrpdzxCVMygS2";
+const storeuid = "OtCqKZEsliZHUhvkrpdzxCVMygS2";
 const storePhoneNum = "+20 1097 755 498";
 const storePhoneNum2 = "";
 const storeEmail = "yassinsalman2007@gmail.com";
