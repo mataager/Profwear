e152b5c6b5e3f812e1c948a3b9c72ea38ca43e86
mataager <omarbussmo077@gmail.com>
2024-12-27 20:38:09 +0200

    Update static.js

--- assets/js/static.js
+++ assets/js/static.js
@@ -8,7 +8,7 @@ const storePhoneNum2 = "";
 const storeEmail = "yassinsalman2007@gmail.com";
 const storeGender = [];
 const storeType = "clothing"; // clothing or footwear
-const freeshipping = "2000";
+const freeshipping = "2500";
 //images url
 const pannerurl = "https://i.imgur.com/TwU6xKz.jpeg";
 //
