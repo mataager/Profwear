9229472bb70a3ea11a7a3d723ae16be690dfedd5
mataager <omarbussmo077@gmail.com>
2025-06-17 16:00:21 +0300

    Update static.js

--- manage/js/static.js
+++ manage/js/static.js
@@ -8,8 +8,8 @@ let maxStock = 50; // Total stock
 const storetitle = "Prof Dashboard";
 const shippingAapiKey = "8296fhh74t";
 //bunny data
-const accessKey = "";
-const storageZoneName = "";
-const pullZone = "";
+const accessKey = "386f137f-26df-4cde-98c24580e6df-d5b8-4441";
+const storageZoneName = "prof-store";
+const pullZone = "profwear.b-cdn.net";
 document.getElementById("title").innerHTML = storetitle;
 //
